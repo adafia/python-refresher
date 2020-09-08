4e1e35821bdc537146e0b02cb076874bfeb8fbc2
adafia <adafia.samuel@gmail.com>
2020-09-08 19:22:22 +0200

    positional and keyword arguments

---
+++ functions/divide.py
@@ -0,0 +1,5 @@
+def divide(dividend, divisor):
+  if divisor != 0:
+    print(dividend / divisor)
+  else:
+    print("Nope")
---
+++ functions/say_hello.py
@@ -0,0 +1,9 @@
+def say_hello(name, surname):
+  print(f"Hello, {name} {surname}")
+
+
+# positional arguments
+say_hello("Edem", "Adafia")
+
+# key word arguments
+say_hello(surname="Adafia", name="Edem")
